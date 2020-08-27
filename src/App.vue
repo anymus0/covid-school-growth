120efef001d6ee05e53589828e5e5385a80f298f
Balazs Nyirfa <balazsnyirfa@gmail.com>
2020-08-27 20:04:01 +0200

    refactor the difference component & add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,67 @@
 <template>
-  <div id="app">
-    <h2>How much has covid infections grown since the new school year has started?</h2>
-    <InfectionsSinceSchool :latest="latestByCountry" />
-    <h2>Summer data</h2>
-    <InfectionsBeforeSchool />
-  </div>
+  <v-app id="app" dark>
+    <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-toolbar-title>Covid19 growth tracker</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col class="text-center">
+            <h2>How much has covid infections grown since the new school year has started?</h2>
+            <differenceOfInfections
+              :date1="latestByCountry"
+              :date2="whenSchoolStarted"
+            />
+            <h2>Between the quarantine has started and ended:</h2>
+            <differenceOfInfections
+              :date1="whenSchoolStarted"
+              :date2="whenQuarantineStarted"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer
+      color="indigo"
+      app
+    >
+      <span class="white--text">Origin of data: Hungary</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import InfectionsSinceSchool from './components/InfectionsSinceSchool'
-import InfectionsBeforeSchool from './components/InfectionsBeforeSchool'
+import differenceOfInfections from './components/differenceOfInfections'
 import {Fetchy} from './Fetch'
 
 export default {
   name: 'App',
   components: {
-    InfectionsSinceSchool,
-    InfectionsBeforeSchool
+    differenceOfInfections
   },
   data() {
     return {
-      latestByCountry: {}
+      latestByCountry: {},
+      whenSchoolStarted: {},
+      whenQuarantineStarted: {}
     }
   },
   methods: {
     async init() {
       this.latestByCountry = await Fetchy.Get('https://covid19-api.org/api/status/hu')
+      this.whenSchoolStarted = await Fetchy.Get('https://covid19-api.org/api/status/hu?date=2020-08-26')
+      this.whenQuarantineStarted = await Fetchy.Get('https://covid19-api.org/api/status/hu?date=2020-04-01')
     }
   },
   mounted() {
@@ -42,7 +78,5 @@ export default {
   text-align: center;
   color: #b4e4fa;
   background-color: #1b1b1b;
-  margin-top: 1rem;
-  padding: 0.7rem;
 }
 </style>
---
+++ src/components/differenceOfInfections.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p class="value"><strong>{{ percentageOfDifference }}%</strong></p>
+    <p>In numbers:</p>
+    <p class="value"><strong>{{ numOfDifference }}</strong></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'differenceOfInfections',
+  props: ['date1', 'date2'],
+  computed: {
+    numOfDifference() {
+      return this.date1.cases - this.date2.cases
+    },
+    percentageOfDifference() {
+      return (((this.date1.cases - this.date2.cases) / ((this.date1.cases + this.date2.cases) / 2)) * 100).toFixed(1)
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.value {
+  color: rgba(214, 87, 87, 0.815);
+  font-size: 1.3rem;
+}
+</style>
